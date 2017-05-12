ecb6b7764b67d0120b2b2f627661d37b956a95cc
Nemanja Isajlovic <ilanele@hotmail.com>
2017-05-12 09:47:55 +0200

    firist commit by Nemanja

---
+++ PackingMachine/Globals.h
@@ -0,0 +1,16 @@
+/*
+ * Globals.h
+ *
+ * Created: 5.11.2016 7:44:43
+ *  Author: nemanja.isajlovic
+ */ 
+
+
+#ifndef GLOBALS_H_
+#define GLOBALS_H_
+
+
+
+
+
+#endif /* GLOBALS_H_ */
---
+++ PackingMachine/PackingMachine.c
@@ -0,0 +1,20 @@
+/*
+ * PackingMachine.c
+ *
+ * Created: 4.11.2016 10:02:34
+ *  Author: nemanja.isajlovic
+ */ 
+
+
+#include <avr/io.h>
+#include "lcd.h"
+
+int main(void)
+{
+    while(1)
+    {
+		
+		
+        //TODO:: Please write your application code 
+    }
+}
